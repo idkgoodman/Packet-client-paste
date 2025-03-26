6af17ffcb1cfb5a77fb1499f1bd474c2a1a0b957
idkgoodman <leha22182@gmail.com>
2025-03-26 16:39:06 +0300

    Add files via upload

---
+++ src/SDK/Minecraft/Actor/Components/RenderPositionComponent.hpp
@@ -0,0 +1,11 @@
+//
+// Created by vastrakai on 7/7/2024.
+//
+
+#pragma once
+#include <glm/glm.hpp>
+
+
+struct RenderPositionComponent {
+    glm::vec3 mPosition;
+};
---
+++ src/SDK/Minecraft/Actor/Components/StateVectorComponent.hpp
@@ -0,0 +1,14 @@
+//
+// Created by vastrakai on 6/30/2024.
+//
+
+#pragma once
+#include <glm/glm.hpp>
+
+
+
+struct StateVectorComponent {
+    glm::vec3 mPos;
+    glm::vec3 mPosOld;
+    glm::vec3 mVelocity;
+};
