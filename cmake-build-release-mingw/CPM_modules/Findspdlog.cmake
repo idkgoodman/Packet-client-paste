b70625af1b5ab2777b80a8829753aa7787bb1c35
idkgoodman <leha22182@gmail.com>
2025-04-04 17:33:34 +0300

    again cmake shit delete

--- cmake-build-release-mingw/CPM_modules/Findspdlog.cmake
+++
@@ -1,3 +0,0 @@
-include("D:/solstice-master-main/cmake-build-release-mingw/cmake/CPM_0.38.7.cmake")
-CPMAddPackage("NAME;spdlog;GITHUB_REPOSITORY;gabime/spdlog;VERSION;1.9.2")
-set(spdlog_FOUND TRUE)
