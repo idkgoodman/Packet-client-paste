2c227bf15646ab1e410adc677b536b0dcb5b0c8b
idkgoodman <leha22182@gmail.com>
2025-04-04 17:40:05 +0300

    Update Criticals.hpp like packet

--- src/Features/Modules/Combat/Criticals.hpp
+++ src/Features/Modules/Combat/Criticals.hpp
@@ -1,15 +1,18 @@
-//
-// Created by alteik on 04/10/2024.
-//
+#pragma once
+
 #include <Features/Modules/Module.hpp>
 #include <Features/Modules/Setting.hpp>
-#pragma once
 
 class Criticals : public ModuleBase<Criticals> {
 public:
-
     enum class Mode {
-        Sentinel,
+        Vanilla,
+    };
+
+    enum class BypassMode {
+        Packet,
+        Motion,
+        Hybrid
     };
 
     enum class AnimationState {
@@ -20,40 +23,12 @@ public:
         FINISHED
     };
 
-    EnumSettingT<Mode> mMode = EnumSettingT<Mode>("Mode", "The critical mode", Mode::Sentinel, "Sentinel");
-    BoolSetting mVelocity = BoolSetting("Use Velocity", "Whether to use velocity change", true);
-    BoolSetting mPositionChange = BoolSetting("Position Change", "Whether to change pos", true);
-    BoolSetting mBiggerPositionChange = BoolSetting("Bigger Position Change", "Whether to change more pos", true);
-    BoolSetting mSendJumping = BoolSetting("Send Jumping", "Whether to send jumping", true);
-    BoolSetting mOffSprint = BoolSetting("Disable Sprint", "Whether to disable sprint", true);
-    NumberSetting mPositionChangePersent = NumberSetting("Position Change Percent", "Changes delta", 1.5, 0, 2, 0.01);
+    Criticals();  // Конструктор модуля
+    void onTick() override;  // Метод, который будет вызываться каждый тик игры
 
-    Criticals() : ModuleBase("Criticals", "Makes you always get a critical hit on your opponent", ModuleCategory::Combat, 0, false) {
-
-        addSetting(&mMode);
-        addSetting(&mVelocity);
-        addSetting(&mPositionChange);
-        addSetting(&mBiggerPositionChange);
-        addSetting(&mSendJumping);
-        addSetting(&mOffSprint);
-        addSetting(&mPositionChangePersent);
-
-        mNames = {
-                {Lowercase, "criticals"},
-                {LowercaseSpaced, "criticals"},
-                {Normal, "Criticals"},
-                {NormalSpaced, "Criticals"}
-        };
-    }
-
-    bool mWasSprinting = true;
-    AnimationState mAnimationState = AnimationState::START;
-
-    float mJumpPositions[4] = {0, 0.8200100660324097 - 0.6200100183486938, 0.741610050201416 - 0.6200100183486938, 0}; // glide down with 0.01 persision
-    float mJumpPositionsMini[4] = {0, 0.02, 0.01, 0}; // 0.05
-    float mJumpVelocities[4] = {-0.07840000092983246, -0.07840000092983246, -0.1552319973707199f, -0.07840000092983246};
-
-    void onEnable() override;
-    void onDisable() override;
-    void onPacketOutEvent(class PacketOutEvent& event);
+private:
+    void applyCritical();  // Метод для применения критического удара
+    void applyPacketCritical();  // Метод для обработки критов через пакеты
+    void applyMotionCritical();  // Метод для обработки критов через движение
+    void applyHybridCritical();  // Метод для комбинированного критового удара
 };
